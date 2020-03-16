b4288eef017285edc899bf335e275fd13e562830
heliks <teh4.ews@gmail.com>
2020-03-16 21:04:33 +0100

    docs(renderer): add missing doc blocks

--- packages/pixi/src/sprite/sprite-animation.ts
+++ packages/pixi/src/sprite/sprite-animation.ts
@@ -1,9 +1,29 @@
 export class SpriteAnimation {
 
+  /** Index of the current frame. */
   public frame = 0;
+
+  /**
+   * The duration that each frame is displayed before switching to
+   * the next (in MS).
+   */
   public speed = 100;
+
+  /** Elapsed time since the animation has started. */
   public elapsedTime = -1;
 
+  /**
+   * @param frames An array containing the Ids of all sprites that make
+   *  up the sprite animation.
+   */
   constructor(public readonly frames: number[]) {}
+
+  /** Sets the [[frames]] that are used to create the animation. */
+  public setFrames(frames: number[]): this {
+    this.frames.length = 0;
+    this.frames.push(...frames);
+
+    return this;
+  }
 
 }
