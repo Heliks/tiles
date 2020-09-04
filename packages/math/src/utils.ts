88e58f9220a5162b9f060340f17827fcc7ea91ce
heliks <teh4.ews@gmail.com>
2020-09-04 23:54:46 +0200

    fix(math): rand() returns floats, not ints

--- packages/math/src/utils.ts
+++ packages/math/src/utils.ts
@@ -1,5 +1,5 @@
-/** Returns a random int between `min` and `max`. */
+/** Returns a random float between `min` and `max`. */
 export function rand(max: number, min = 0): number {
-  return Math.random() * (max - min + 1) + min
+  return Math.random() * (max - min) + min
 }
 
