f1d866be6009cde71d1d52b27fca5323d6e05f47
heliks <teh4.ews@gmail.com>
2020-09-04 08:09:48 +0200

    refactor(math): move math into its own module

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,18 +1,14 @@
 export * from './entity-system';
 export * from './game';
 export * from './game-builder';
-export * from './math';
 export * from './state'
 export * from './ticker';
 export * from './types';
 export * from './utils';
 
+// Re-export core-packages.
 export * from '@heliks/event-queue';
 export * from '@heliks/hierarchy';
-export * from '@heliks/transform';
-
-// Re-export the injector so that it can be used by modules without the need for peer
-// dependencies, as it'll be required by most packages anyway.
+export * from '@heliks/tiles-math';
 export * from '@heliks/tiles-injector';
-
-
+export * from '@heliks/transform';
