2882981ba0b314e01a02d39430b4a737a0c3f023
heliks <teh4.ews@gmail.com>
2020-10-03 19:15:37 +0200

    feat(math): add Circle class

--- packages/math/src/rectangle.ts
+++ packages/math/src/rectangle.ts
@@ -1,9 +1,22 @@
 /** A rectangle shape. */
 export class Rectangle {
 
+  /**
+   * @param width The rectangles width.
+   * @param height The rectangles height.
+   * @param x (optional) Position on x axis.
+   * @param y (optional) Position on y axis.
+   */
   constructor(
-    public readonly width: number,
-    public readonly height: number
+    public width: number,
+    public height: number,
+    public x = 0,
+    public y = 0
   ) {}
+
+  /** Creates a new `Rectangle` from this one. */
+  public copy(): Rectangle {
+    return new Rectangle(this.width, this.height, this.x, this.y);
+  }
 
 }
