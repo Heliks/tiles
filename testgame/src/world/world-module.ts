f0e3a98f821e3b6cd8acdc954b08dd45bd035be9
felix <teh4.ews@gmail.com>
2021-04-25 23:20:52 +0000

    feat(testgame): support custom object types in maphandler

--- testgame/src/world/world-module.ts
+++ testgame/src/world/world-module.ts
@@ -2,12 +2,27 @@ import { GameBuilder } from '@heliks/tiles-engine';
 import { MapManager } from './map-manager';
 import { MapHierarchy } from './map-hierarchy';
 import { MapSpawner } from './map-spawner';
+import { ObjectType, ObjectTypes } from './object-types';
 
 export class WorldModule {
+
+  /** @internal */
+  private readonly objectTypes = new ObjectTypes();
+
+  /** Registers an object type. */
+  public use(type: string, klass: ObjectType): this {
+    this.objectTypes.set(type, klass);
+
+    return this;
+  }
 
   /** @inheritDoc */
   public build(builder: GameBuilder): void {
     builder
+      .provide({
+        token: ObjectTypes,
+        value: this.objectTypes
+      })
       .provide(MapHierarchy)
       .provide(MapSpawner)
       .provide(MapManager);
