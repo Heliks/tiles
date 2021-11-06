38ddf39e264af061492b000f2a7db8f512c4e162
felix <teh4.ews@gmail.com>
2021-11-06 06:40:33 +0100

    fix(engine): pre-register Transform component in TransformModule

--- packages/engine/src/transform/transform-module.ts
+++ packages/engine/src/transform/transform-module.ts
@@ -2,10 +2,11 @@ import { GameBuilder, Module } from '../game-builder';
 import { Hierarchy } from '@heliks/ecs-hierarchy';
 import { HierarchySystem } from './hierarchy-system';
 import { TransformSystem } from './transform-system';
+import { Transform } from '@heliks/ecs-transform';
 
 /**
- * Module that provides a simple hierarchical transform system.
- * Note: You do not need this module to use the `Transform` component independently.
+ * Optional module that provides a simple hierarchical transform system. This module
+ * is not required to use the `Transform` component independently.
  */
 export class TransformModule implements Module {
 
@@ -14,6 +15,7 @@ export class TransformModule implements Module {
     const hierarchy = new Hierarchy();
 
     builder
+      .component(Transform)
       .provide({
         token: Hierarchy,
         value: hierarchy
