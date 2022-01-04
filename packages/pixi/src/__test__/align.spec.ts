9da90680c57dfa2dc7806b5b265f7e8c600b944c
felix <teh4.ews@gmail.com>
2022-01-04 16:09:16 +0100

    test(pixi): fix unit test

--- packages/pixi/src/__test__/align.spec.ts
+++ packages/pixi/src/__test__/align.spec.ts
@@ -1,4 +1,4 @@
-import { Vec2, vec2 } from '@heliks/tiles-engine';
+import { Vec2 } from '@heliks/tiles-engine';
 import { Align, alignTo } from '../align';
 
 
@@ -7,19 +7,22 @@ describe('Align', () => {
 
   beforeEach(() => {
     // Note: We always assume that the input position is aligned as Align.Center.
-    pos = vec2(0, 0);
+    pos = new Vec2(0, 0);
   });
 
   test.each([
-    { name: 'left',         align: Align.Left,        expected: vec2(-5, 0) },
-    { name: 'right',        align: Align.Right,       expected: vec2(5, 0) },
-    { name: 'top',          align: Align.Top,         expected: vec2(0, -5) },
-    { name: 'top left',     align: Align.TopLeft,     expected: vec2(-5, -5) },
-    { name: 'top right',    align: Align.TopRight,    expected: vec2(5, -5) },
-    { name: 'bottom',       align: Align.Bottom,      expected: vec2(0, 5) },
-    { name: 'bottom left',  align: Align.BottomLeft,  expected: vec2(-5, 5) },
-    { name: 'bottom right', align: Align.BottomRight, expected: vec2(5, 5) },
+    { name: 'left',         align: Align.Left,        expected: new Vec2(-5, 0) },
+    { name: 'right',        align: Align.Right,       expected: new Vec2(5, 0) },
+    { name: 'top',          align: Align.Top,         expected: new Vec2(0, -5) },
+    { name: 'top left',     align: Align.TopLeft,     expected: new Vec2(-5, -5) },
+    { name: 'top right',    align: Align.TopRight,    expected: new Vec2(5, -5) },
+    { name: 'bottom',       align: Align.Bottom,      expected: new Vec2(0, 5) },
+    { name: 'bottom left',  align: Align.BottomLeft,  expected: new Vec2(-5, 5) },
+    { name: 'bottom right', align: Align.BottomRight, expected: new Vec2(5, 5) },
   ])('should align to $name', ({ align, expected }) => {
-    expect(alignTo(pos, 10, 10, align)).toMatchObject(expected);
+    const position = alignTo(pos, 10, 10, align);
+
+    expect(position.x).toBe(expected.x);
+    expect(position.y).toBe(expected.y);
   });
 });
