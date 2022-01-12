799e317086b76ca014f745b824a948364e1595be
felix <teh4.ews@gmail.com>
2022-01-12 04:40:15 +0100

    refactor(physics): change payload of contact events

--- packages/physics-box2d/src/box2d-contact-listener.ts
+++ packages/physics-box2d/src/box2d-contact-listener.ts
@@ -1,7 +1,7 @@
 /* eslint-disable new-cap */
 import { b2Contact, b2ContactListener } from '@flyover/box2d';
-import { ContactEvent, ContactEvents, RigidBody } from '@heliks/tiles-physics';
-import { Entity, World } from '@heliks/tiles-engine';
+import { World } from '@heliks/tiles-engine';
+import { ContactEvent, ContactEvents } from '@heliks/tiles-physics';
 
 
 /** Listens to Box2D contact events and forwards them to an `EventQueue`. */
@@ -12,30 +12,6 @@ export class Box2dContactListener extends b2ContactListener {
     protected readonly world: World
   ) {
     super();
-  }
-
-  /** @internal */
-  private push(a: Entity, b: Entity, type: ContactEvent): void {
-    const storage = this.world.storage(RigidBody);
-
-    const bodyA = storage.get(a);
-    const bodyB = storage.get(b);
-
-    this.queue.push({
-      bodyA,
-      bodyB,
-      entityA: a,
-      entityB: b,
-      type
-    });
-
-    this.queue.push({
-      bodyA: bodyB,
-      bodyB: bodyA,
-      entityA: b,
-      entityB: a,
-      type
-    });
   }
 
   /** @inheritDoc */
@@ -52,7 +28,13 @@ export class Box2dContactListener extends b2ContactListener {
     colliderA.collider.addContact(entityB, colliderB.collider.id);
     colliderB.collider.addContact(entityA, colliderA.collider.id);
 
-    this.push(entityA, entityB, ContactEvent.Begin);
+    this.queue.push({
+      colliderA,
+      colliderB,
+      entityA,
+      entityB,
+      type: ContactEvent.Begin
+    });
   }
 
   /** @inheritDoc */
@@ -69,13 +51,13 @@ export class Box2dContactListener extends b2ContactListener {
     colliderA.collider.removeContact(entityB, colliderB.collider.id);
     colliderB.collider.removeContact(entityA, colliderA.collider.id);
 
-    // Todo: This is a race-condition with the entity world that already cleaned up the
-    //  rigid bodies. This should be fixed because as of right now there is no
-    //  ContactEvent.End if the entity is destroyed during the contact, but that
-    //  information might be useful for certain systems.
-    if (this.world.alive(entityA) && this.world.alive(entityB)) {
-      this.push(entityA, entityB, ContactEvent.End);
-    }
+    this.queue.push({
+      colliderA,
+      colliderB,
+      entityA,
+      entityB,
+      type: ContactEvent.End
+    });
   }
 
 }
