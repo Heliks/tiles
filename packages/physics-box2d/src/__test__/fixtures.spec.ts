225ce052445eb35cf415a83e4866d9d03dbe9b5c
felix <teh4.ews@gmail.com>
2022-02-06 02:29:40 +0100

    feat(physics-box2d): synchronize colliders with fixtures

---
+++ packages/physics-box2d/src/__test__/fixtures.spec.ts
@@ -0,0 +1,40 @@
+import { b2World } from '@flyover/box2d';
+import { Rectangle } from '@heliks/tiles-engine';
+import { Collider, MaterialManager, RigidBody } from '@heliks/tiles-physics';
+import { Box2dBodyFactory } from '../box2d-body-factory';
+import { getColliderFixture, syncBodyFixtures } from '../fixtures';
+
+
+describe('synchronize fixtures', () => {
+  const factory = new Box2dBodyFactory(
+    new b2World({
+      x: 0,
+      y: 0
+    }),
+    new MaterialManager()
+  );
+
+  it('should update collision filters', () => {
+    const collider = new Collider(new Rectangle(
+      0,
+      0
+    ));
+
+    const body = factory.createBody(
+      0,
+      new RigidBody().attach(collider)
+    );
+
+    const group = 1 | 2 | 3
+    const mask  = 2 | 3;
+
+    collider.setFilterData(group, mask);
+
+    syncBodyFixtures(body);
+
+    const filters = getColliderFixture(body, collider)?.GetFilterData();
+
+    expect(filters?.categoryBits).toBe(group);
+    expect(filters?.maskBits).toBe(mask);
+  });
+});
---
+++ packages/physics-box2d/src/fixtures.ts
@@ -0,0 +1,36 @@
+import { b2Body, b2Fixture } from '@flyover/box2d';
+import { Collider } from '@heliks/tiles-physics';
+
+
+export function syncBodyFixtures(body: b2Body): void {
+  for (let fixture = body.GetFixtureList(); fixture; fixture = fixture.GetNext()) {
+    const collider = fixture.GetUserData().collider;
+
+    if (collider.isDirty) {
+      // Update collision filters.
+      const filter = fixture
+        .GetFilterData()
+        .Clone();
+
+      filter.categoryBits = collider.group;
+      filter.maskBits = collider.mask;
+
+      fixture.SetFilterData(filter);
+
+      // Clear flag.
+      collider.isDirty = false;
+    }
+  }
+}
+
+/**
+ * Returns the `b2Fixture` on `body` that belongs to the given `collider`, or nothing if
+ * no matching fixture could be found on that body.
+ */
+export function getColliderFixture(body: b2Body, collider: Collider): b2Fixture | undefined {
+  for (let fixture = body.GetFixtureList(); fixture; fixture = fixture.GetNext()) {
+    if (fixture.GetUserData().collider.id === collider.id) {
+      return fixture;
+    }
+  }
+}
