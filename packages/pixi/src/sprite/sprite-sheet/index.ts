105d3835a29858cd8bbd90e1adcb43c9091f0a73
felix <teh4.ews@gmail.com>
2022-02-21 21:01:21 +0100

    refactor(pixi): normalize name of asset loader formats

--- packages/pixi/src/sprite/sprite-sheet/index.ts
+++ packages/pixi/src/sprite/sprite-sheet/index.ts
@@ -2,6 +2,6 @@ export * from './packed-sprite';
 export * from './packed-sprite-sheet';
 export * from './sprite-grid';
 export * from './sprite-sheet';
-export * from './sprite-sheet-format';
-export * from './image-to-sprite-sheet';
+export * from './load-sprite-sheet';
+export * from './load-sprite-sheet-from-image';
 export * from './sprite-sheet-storage';
