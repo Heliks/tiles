360c1ab3292d556cb4abb6a4863624cba19db8ed
felix <teh4.ews@gmail.com>
2022-02-21 21:04:51 +0100

    test(physics): remove deprecated tests

--- packages/physics/src/__test__/collider.spec.ts
+++ packages/physics/src/__test__/collider.spec.ts
@@ -5,26 +5,6 @@ import { Collider } from '../collider';
 const RECT = new Rectangle(0, 0, 0, 0);
 
 describe('Collider', () => {
-  it('should add contacts with other colliders', () => {
-    const collider = new Collider(RECT);
-
-    collider.addContact(0, 15);
-
-    expect(collider.contacts[0]).toMatchObject({
-      colliderId: 15,
-      entity: 0
-    });
-  });
-
-  it('should remove contacts with other colliders', () => {
-    const collider = new Collider(RECT);
-
-    collider.addContact(0, 15);
-    collider.removeContact(0, 15);
-
-    expect(collider.contacts).toHaveLength(0);
-  });
-
   describe('tags', () => {
     const TAG_A = 1;
     const TAG_B = 2;
