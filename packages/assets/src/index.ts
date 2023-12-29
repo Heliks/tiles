08e12df30b3e951dc0172c30e16dd34f721a28c4
felix <teh4.ews@gmail.com>
2023-12-29 08:25:25 +0100

    feat(assets): add default format for JSON files

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -2,4 +2,5 @@ export * from './asset';
 export * from './asset-loader';
 export * from './assets-bundle';
 export * from './format';
+export * from './load-json';
 export * from './utils';
---
+++ packages/assets/src/load-json.ts
@@ -0,0 +1,23 @@
+import { Format } from './format';
+
+
+/**
+ * Default format to load .json files.
+ *
+ * This does no additional parsing and will return the loaded JSON as is.
+ */
+export class LoadJSON implements Format<object, object> {
+
+  /**
+   * @inheritDoc
+   */
+  readonly extensions = ['json'];
+
+  /**
+   * @inheritDoc
+   */
+  public process(data: object): object {
+    return data;
+  }
+
+}
