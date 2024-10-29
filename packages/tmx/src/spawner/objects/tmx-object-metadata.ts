47ab313be02ea97fbb55606ca76a7cb1be8979f8
felix <teh4.ews@gmail.com>
2024-10-29 21:57:26 +0100

    feat(tmx): expose original properties in TmxObjectMetadata

--- packages/tmx/src/spawner/objects/tmx-object-metadata.ts
+++ packages/tmx/src/spawner/objects/tmx-object-metadata.ts
@@ -1,15 +1,14 @@
-/**
- * Component that carries Tiled meta-information.
- *
- * This component is attached to all entities that are created from objects that are
- * placed on object layers.
- */
-export class TmxObjectMetadata {
+import { HasProperties } from '../../parser';
+
+
+/** Carries metadata about the TMX object from which this entity was spawned. */
+export class TmxObjectMetadata<P = unknown> implements HasProperties<P> {
 
   /**
-   * @param id Unique Id.
-   * @param name Custom name assigned to the object.
+   * @param id Tiled internal object ID.
+   * @param properties Custom properties.
+   * @param name Custom name.
    */
-  constructor(public readonly id: number, public readonly name?: string) {}
+  constructor(public readonly id: number, public readonly properties: P, public readonly name?: string) {}
 
 }
