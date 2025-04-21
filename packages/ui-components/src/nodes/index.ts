31948abb3d9c6e7c8368a3ebb9dc88dcbacc56dc
felix <teh4.ews@gmail.com>
2025-04-21 19:56:17 +0200

    refactor(ui-components): use <sprite> tag for animated sprites too

--- packages/ui-components/src/nodes/index.ts
+++ packages/ui-components/src/nodes/index.ts
@@ -3,6 +3,5 @@ export * from './fill';
 export * from './slice-plane';
 export * from './span';
 export * from './sprite';
-export * from './sprite-animation';
 export * from './text';
 export * from './texture';
